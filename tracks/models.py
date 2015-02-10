aaec58509fe33c72a1eb1ccce205711b34f9a5e1
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-02-10 11:06:02 -0500

    Se modifica el modelo track para corregir el comportamiento del reproductor de audio insertado en el panel administrador

--- tracks/models.py
+++ tracks/models.py
@@ -1,27 +1,26 @@
 from django.db import models
-from artists.models import Artist
+
 from albums.models import Album
-
-
-# Create your models here.
+from artists.models import Artist
 
 class Track(models.Model):
-	title 		= models.CharField(max_length=255)
-	order 		= models.PositiveIntegerField()
-	track_file	= models.FileField(upload_to='tracks')
-	artist		= models.ForeignKey(Artist)
-	album 		= models.ForeignKey(Album)
+	title = models.CharField(max_length=255)
+	order = models.PositiveIntegerField()
+	track_file = models.FileField(upload_to="tracks")
+	album = models.ForeignKey(Album)
+	artist = models.ForeignKey(Artist)
 
 	def get_absolute_url(self):
 		return '/tracks/%s/' % self.title
 
 	def player(self):
-		return """ 
+		return """
 		<audio controls>
-			<source src="%s" type="audio/mpeg>
-			Your browser does not support the audio tag 
+			<source src="%s" type="audio/mpeg">
+			Your browser does not support the audio tag.
 		</audio>
 		""" % self.track_file.url
+
 	player.allow_tags = True
 	player.admin_order_field = 'track_file'
 
